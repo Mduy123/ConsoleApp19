f9589cb99470eecf2510ebba94dbea485eb25fc6
admin <admin@MDUY>
2024-01-19 11:02:36 +0700

    Add project files.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           tuong flotino = new tuong("flotino", 100, 100, 100);
+            flotino.ten = "flotino";
+            flotino.tancong = 100;
+            flotino.phongthu = 100;
+            flotino.HP = 100;
+
+            flotino.chieu1();
+
+            tuong bijan = new tuong();
+            bijan.ten = "flotino";
+            bijan.tancong = 100;
+            bijan.phongthu = 500;
+            bijan.HP = 100;
+            bijan.HPchange(flotino.tancong, bijan.phongthu);
+
+            var aya = new tuong("aya", 900);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp19/tuong.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    public class tuong
+    {
+        public string ten { get; set; }
+        public int tancong { get; set; }
+        public int phongthu { get; set;}
+        public int HP { get; set; }
+
+        public tuong()
+        {
+            
+        }
+
+        public tuong(String ten, int HP)
+        {
+            this.ten = ten;
+            this.HP = HP;
+        }
+
+        public tuong(string ten)
+        {
+            this.ten = ten;
+        }
+
+        public tuong(string ten, int tancong, int phongthu, int HP)
+        {
+            this.ten = ten;
+            this.tancong = tancong;
+            this.phongthu = phongthu;
+            this.HP = HP;
+        }
+        public void chieu1()
+        {
+           this.tancong *= 2;
+        }
+
+        public void HPchange(int tancong, int phongthu)
+        {
+            this.HP -= (tancong - phongthu);
+        }
+    }
+}
